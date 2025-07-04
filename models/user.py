cd20e9ad7c1ad25166c53dc80106f3025d2dfa4e
Trần Thế Hảo <147327094+050903@users.noreply.github.com>
2025-07-04 20:37:17 +0700

    Fix: set __tablename__ = 'users' in User model

--- models/user.py
+++ models/user.py
@@ -7,6 +7,7 @@ from flask import current_app
 from database import db, login_manager
 
 class User(UserMixin, db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
